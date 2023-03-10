9f5231179c230e6ec06bab08d3bb7ff468a4f571
sunwenqi <sunwenqi@yushu.biz>
2023-03-10 15:13:23 +0800

    upload

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,19 +1,74 @@
 <!--  -->
 <template>
-  <div class="search">
-    <input type="text" @keydown="queryFundInfo" v-model="fundModel" />
+  <div class="search" ref="search">
+    <div class="list" v-show="visibility">
+      <div class="item" v-for="item in list">
+        <span class="name">{{ item.name }}</span>
+        <span class="code">{{ item.code }}</span>
+      </div>
+    </div>
+    <input type="text" v-model="fundModel" @focus="searchFocus" />
   </div>
 </template>
 
 <script setup>
 import { defineComponent, useCssModule, reactive, ref } from "vue";
+import _ from "lodash";
+import { asyncComputed, useDebounce, onClickOutside } from "@vueuse/core";
 import * as api from "../script/api";
-const fundModel = ref();
-// 股票、港股、基金、美股、英股
 
-const queryFundInfo = async () => {
-  const res = await api.getFuzzyList(fundModel.value)
+const fundModel = ref("");
+const debounced = useDebounce(fundModel, 400)
+const visibility = ref(false)
+
+const list = asyncComputed(async () => {
+  const result = []
+  if (debounced.value == "") return result;
+  const [Astock, HKstock, fund, stock] = await api.getFuzzyList(debounced.value);
+  fund.forEach(({ name, code }) => {
+    result.push({
+      code,
+      name,
+      type: "fund"
+    })
+  })
+  Astock.forEach(({ code, name }) => {
+    result.push({
+      code,
+      name,
+      type: "stock"
+    })
+  });
+  HKstock.forEach(({ name, thscode }) => {
+    result.push({
+      code: thscode,
+      name,
+      type: "stock"
+    })
+  })
+  stock.forEach(({ name, code }) => {
+    result.push({
+      code,
+      name,
+      type: "stock"
+    })
+  })
+  visibility.value = true;
+  return _.sortedUniq(result)
+}, null)
+
+const search = ref(null);
+onClickOutside(search, (event) => {
+  visibility.value = false;
+})
+
+const searchFocus = () => {
+  if (debounced.value) {
+    visibility.value = true;
+  }
 }
+
+
 
 </script>
 <style lang='scss' scoped>
@@ -24,11 +79,34 @@ const queryFundInfo = async () => {
   height: 30px;
   border-top: 1px solid #e3e3e3;
 
+  .list {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    bottom: 31px;
+    line-height: 30px;
+    border-top: 1px solid #e3e3e3;
+
+    .item {
+      display: flex;
+      flex-grow: 1;
+      justify-content: space-between;
+      cursor: pointer;
+      padding: 0 5px;
+
+      &:hover {
+        background: #e3e3e3;
+      }
+    }
+  }
+
   input {
     display: flex;
     flex-grow: 1;
     border: none;
     outline: none;
+    padding: 0 5px;
   }
 }
 </style>
